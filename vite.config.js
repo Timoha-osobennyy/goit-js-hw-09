ce0dbb1936c0ed8754ef51e834f671a7bf04ab9e
Timofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-05-14 07:33:06 +0300

    JSKS

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-09/', // 👈 ОБЯЗАТЕЛЬНО ДЛЯ GITHUB PAGES
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
