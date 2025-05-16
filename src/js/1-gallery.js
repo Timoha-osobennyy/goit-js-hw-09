0e48bdb56d10114697148420a0dd2ab15433d9ac
Timofii <141590304+Timoha-osobennyy@users.noreply.github.com>
2025-05-16 22:34:45 +0300

    оіі09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,7 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'Simplelightbox/dist/simple-lightbox.min.css';
 
-// Array of gallery images
 const images = [
   {
     preview:
